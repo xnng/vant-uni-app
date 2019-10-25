c5b58d05c44bd1ed3155d9aeb4ad0b2041c76399
xnng <xnng77@gmail.com>
2019-10-25 13:53:56 +0800

    style: update button component

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,15 @@
 <template>
   <view>
-    <van-button type="default">按钮</van-button>
+    <view class="title">按钮类型</view>
+    <van-button class="btn" type="default">默认按钮</van-button>
+    <van-button class="btn" type="primary">主要按钮</van-button>
+    <van-button class="btn" type="info">信息按钮</van-button>
+    <van-button class="btn" type="danger">危险按钮</van-button>
+    <van-button class="btn" type="warning">警告按钮</van-button>
+
+    <view class="title">朴素按钮</view>
+    <van-button class="btn" plain type="primary">朴素按钮</van-button>
+    <van-button class="btn" plain type="info">朴素按钮</van-button>
   </view>
 </template>
 
@@ -12,8 +21,21 @@ export default {
     return {
       title: 'Hello'
     }
-  },
-  onLoad() {},
-  methods: {}
+  }
 }
 </script>
+<style lang="scss" scoped>
+.title {
+  font-size: 14px;
+  padding: 25px 15px 5px 15px;
+  color: rgba(69, 90, 100, 0.6);
+}
+.btn {
+  width: 88px;
+  margin-top: 10px;
+  margin-left: 15px;
+}
+</style>
+
+<style lang="scss">
+</style>
